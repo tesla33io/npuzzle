d56b24334093568687a49144364b46ddc8dc1959
tesla33io <artemstavropolov@gmail.com>
2026-02-19 22:15:59 +0100

    feat: add random grid generation

--- src/main.jl
+++ src/main.jl
@@ -1,7 +1,23 @@
 module NPuzzle
+using Random
+import Dates
+
+# Creates a grid representing a random state
+# 0x00 represents the empty tile
+function create_grid(N::UInt8)::Vector{UInt8}
+    grid::Vector{UInt8} = randperm(15)
+    empty_idx::UInt8 = rand(1:15)
+    insert!(grid, empty_idx, 0)
+    return grid
+end # create_grid
 
 function main()
-    println("Hello world")
+    for i = 1:10
+        grid::Vector{UInt8} = create_grid(UInt8(16))
+        println(length(grid))
+        println(grid)
+        sleep(1)
+    end # for
 end # main
 
 end # module
